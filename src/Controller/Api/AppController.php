aaf9cbc48bb8a7cc4310beead7ca0eb2a6ab7359
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-08-16 18:53:15 +0530

    rest api

---
+++ src/Controller/Api/AppController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller\Api;
+
+use Cake\Controller\Controller;
+use Cake\Event\Event;
+
+class AppController extends Controller
+{
+
+    use \Crud\Controller\ControllerTrait;
+
+    public function beforeFilter(Event $event)
+    {
+       
+        parent::beforeFilter($event);
+        $this->response = $this->response->cors($this->request)
+        ->allowOrigin(['*'])
+        ->allowMethods(['GET', 'POST'])
+        ->allowHeaders(['X-CSRF-Token'])
+        ->allowCredentials()
+        ->exposeHeaders(['Link'])
+        ->maxAge(300)
+        ->build();
+    }
+
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('RequestHandler');
+        $this->loadComponent('Crud.Crud', [
+            'actions' => [
+                'Crud.Index',
+                'Crud.View',
+                'Crud.Add',
+                'Crud.Edit',
+                'Crud.Delete'
+            ],
+            'listeners' => [
+                'Crud.Api',
+                'Crud.ApiPagination',
+                'Crud.ApiQueryLog'
+            ]
+        ]);
+            }}
